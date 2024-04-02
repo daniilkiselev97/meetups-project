8122e547dfc48eab29b7192ce9ec946b84e9f5ef
Daniil Kiselev <danqkiselev@yandex.ru>
2024-04-02 16:44:13 +0300

    feat: edit-meetup-inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 import { TuiRootModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import {TuiExpandModule, TuiButtonModule, TuiErrorModule} from '@taiga-ui/core';
-import {TuiInputModule, TuiFieldErrorPipeModule, TuiInputDateModule} from '@taiga-ui/kit';
+import {TuiInputModule, TuiFieldErrorPipeModule, TuiInputDateModule, TuiInputTimeModule, TuiTextAreaModule} from '@taiga-ui/kit';
+
+
+//TUI_INPUT_TIME_OPTIONS
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,7 +19,6 @@ import { UsersComponent } from './pages/users/users.component';
 import { CardMeetupComponent } from './components/card-meetup/card-meetup.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { AuthInterceptor } from "./interceptors/auth.interceptor";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AuthorizationComponent } from "./pages/authorization/authorization.component";
 import { EditMeetupComponent } from './pages/edit-meetup/edit-meetup.component';
 
@@ -38,15 +41,20 @@ import { EditMeetupComponent } from './pages/edit-meetup/edit-meetup.component';
     TuiExpandModule, 
     TuiButtonModule,
 		ReactiveFormsModule,
+		FormsModule,
 		HttpClientModule,
 		TuiInputModule,
 		TuiErrorModule,
 		TuiFieldErrorPipeModule,
-		TuiInputDateModule
+		TuiInputDateModule,
+		TuiInputTimeModule,
+		TuiTextAreaModule
 ],
   providers: [
 		{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer},
-		{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+		{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+		
+		
 	],
   bootstrap: [AppComponent]
 })
--- src/app/pages/edit-meetup/edit-meetup.component.ts
+++ src/app/pages/edit-meetup/edit-meetup.component.ts
@@ -1,18 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TuiDay } from '@taiga-ui/cdk';
+import { TUI_INPUT_TIME_OPTIONS, tuiCreateTimePeriods, tuiInputTimeOptionsProvider } from '@taiga-ui/kit';
 
 @Component({
 	selector: 'app-edit-meetup',
 	templateUrl: './edit-meetup.component.html',
 	styleUrls: ['./edit-meetup.component.css'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	providers: [
+		tuiInputTimeOptionsProvider({
+			icon: 'tuiIconClockLarge',
+			mode: 'HH:MM:SS',
+			itemSize: 's',
+		}),
+	]
 })
 export class EditMeetupComponent {
 	readonly myForm = new FormGroup({
 		title: new FormControl(''),
 		date: new FormControl(new TuiDay(2025, 3, 2)),
-});
+		time: new FormControl(null),
+		location: new FormControl(''),
+		short_description: new FormControl('', Validators.required),
+		detailed_description: new FormControl('', Validators.required),
+		target_audience: new FormControl('Аналитики, тестировщики, зумеры...', Validators.required),
+		what_need_to_know: new FormControl('', Validators.required),
+		what_will_happen: new FormControl('', Validators.required),
+		why_do_i_have_to_come: new FormControl('', Validators.required),
+	});
+
+	items1 = tuiCreateTimePeriods();
 
 
 
