88d98abdc5f5e6a50cf5e226db158eafd2449e08
Daniil Kiselev <danqkiselev@yandex.ru>
2024-05-01 22:12:07 +0300

    Добавлен новый объект с конфигом, который идет вторым аргументом в bootstrapApplication

---
+++ src/app-config.ts
@@ -0,0 +1,46 @@
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { authInterceptor } from "./app/interceptors/auth.interceptor";
+import { provideEffects } from "@ngrx/effects";
+import { provideStore } from "@ngrx/store";
+import { reducers } from "./app/store";
+import { AuthEffects } from "./app/store/auth/auth.effects";
+import { MeetupsEffects } from "./app/store/all-meetups/all-meetups.effects";
+import { MyMeetupsEffects } from "./app/store/myMeetups/myMettups.effects";
+import { UsersEffects } from "./app/store/users/users.effects";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
+import { EnvironmentProviders as MyEnvironmentProviders, InjectionToken, Provider, Sanitizer, importProvidersFrom, isDevMode } from "@angular/core";
+import { provideRouterStore } from "@ngrx/router-store";
+import { provideRouter } from "@angular/router";
+import { TUI_SANITIZER, TuiAlertModule, TuiDialogModule, TuiRootModule } from "@taiga-ui/core";
+import { routes } from "./app/app.routes";
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TUI_DIALOG_CLOSES_ON_BACK } from "@taiga-ui/cdk";
+import { of } from "rxjs";
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+
+export interface EnvironmentProviders {
+  providers: (Provider | InjectionToken<Sanitizer> | any)[];
+}
+
+export const appConfig: EnvironmentProviders = {
+  providers: [
+    provideAnimations(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideStore(reducers),
+    provideEffects([AuthEffects, MeetupsEffects, MyMeetupsEffects, UsersEffects]),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideRouterStore(),
+    importProvidersFrom(TuiRootModule, TuiDialogModule, TuiAlertModule),
+    provideRouter(routes),
+    { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
+    {
+      provide: TUI_DIALOG_CLOSES_ON_BACK,
+      useValue: of(true),
+    },
+  ],
+};
+
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,6 @@
+import { appConfig } from './app-config';
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { of } from 'rxjs';
-import { TUI_DIALOG_CLOSES_ON_BACK } from '@taiga-ui/cdk';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
-import { TUI_SANITIZER, TuiAlertModule, TuiDialogModule, TuiRootModule, } from '@taiga-ui/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { importProvidersFrom, isDevMode } from '@angular/core';
-import {  authInterceptor } from './app/interceptors/auth.interceptor';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { provideRouterStore } from '@ngrx/router-store';
-import { AuthEffects } from './app/store/auth/auth.effects';
-import { reducers } from './app/store';
-// import { httpErrorInterceptor } from './app/interceptors/http-error.interceptor';
-import { MeetupsEffects } from './app/store/all-meetups/all-meetups.effects';
-import { MyMeetupsEffects } from './app/store/myMeetups/myMettups.effects';
-import { UsersEffects } from './app/store/users/users.effects';
 
-bootstrapApplication(AppComponent, {
-	providers: [
-		provideAnimations(),
-		provideHttpClient(withInterceptors([authInterceptor])),
-		
-		provideStore(reducers),
-		provideEffects([AuthEffects, MeetupsEffects, MyMeetupsEffects, UsersEffects]),
-		provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-		provideRouterStore(),
-		importProvidersFrom(TuiRootModule, TuiDialogModule, TuiAlertModule),
-		provideRouter(routes),
-		{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
-		{
-			provide: TUI_SANITIZER,
-			useClass: NgDompurifySanitizer,
-		},
-		{
-			provide: TUI_DIALOG_CLOSES_ON_BACK,
-			useValue: of(true),
-		},
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
 
-	]
-}).catch(err => console.error(err));
